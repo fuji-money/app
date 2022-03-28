64a9397d844ef4d125a39bd84aaa10656480927c
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-03-28 17:49:15 +0000

    add icons to notifications; dry css

--- src/components/Borrow/Form/Exception.svelte
+++ src/components/Borrow/Form/Exception.svelte
@@ -1,12 +1,12 @@
-<p>Not enough funds</p>
+<p class="notification">
+  <img src="/images/exception.svg" alt="exception icon">
+  Not enough funds
+</p>
 
 <style>
   p {
     background-color: #FFDEDF;
     border: 1px solid #F18C95;
-    color: #FF4E53;
-    font-size: 0.8rem;
-    font-weight: 700;
-    padding: 4px 20px;
+    color: #E34800;
   }
 </style>
--- src/components/Borrow/Form/Warning.svelte
+++ src/components/Borrow/Form/Warning.svelte
@@ -1,12 +1,12 @@
-<p>Ratio is not safe</p>
+<p class="notification">
+  <img src="/images/warning.svg" alt="warning icon">
+  Ratio is not safe
+</p>
 
 <style>
   p {
     background-color: #FFE8D3;
     border: 1px solid #FF712C;
     color: #FF712C;
-    font-size: 0.8rem;
-    font-weight: 700;
-    padding: 4px 20px;
   }
 </style>
