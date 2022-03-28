dcc3035e57c4e5401db720feefe0ed8c0450a154
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-03-28 19:08:28 +0000

    refactor Topup form

---
+++ src/components/Topup/Form/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Asset } from '../../../lib/types';
+
+  import { notEnoughFunds } from '../../../lib/utils';
+
+  export let balance: Asset[];
+  export let collateral: Asset;
+  export let wallet: boolean;
+
+  $: disabled =
+    !wallet ||
+    notEnoughFunds({ asset: collateral, balance }) ||
+    collateral.quantity === 0;
+</script>
+
+<div class="has-text-centered">
+  <button on:click class="button is-primary is-cta" {disabled}>Topup</button>
+</div>
