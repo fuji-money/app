a683fb8bcc2c78c3cf43337f1650735ee0720a14
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-05-09 17:07:54 +0000

    works on multiply

---
+++ components/multiply/offer.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const MultiplyOffer = () => {
+  return (
+    <div>
+      <div className="is-box mx-auto mt-6">
+        <div className="icon-container is-justify-content-center level">
+          <Image
+            src="/images/assets/btc.svg"
+            alt="Bitcoin logo"
+            height={60}
+            width={60}
+          />
+          &nbsp;
+          <Image
+            src="/images/icons/arrow.svg"
+            alt="Arrow right"
+            height={30}
+            width={30}
+          />
+          <Image
+            src="/images/assets/lbtc.svg"
+            alt="Liquid Bitcoin logo"
+            height={60}
+            width={60}
+          />
+        </div>
+        <h2 className="has-text-centered">BTC-L</h2>
+        <p className="has-text-centered is-size-7 mt-4 mb-5 px-4">
+          Only the collateral will be locked on the Liquid Network, redeemable
+          anytime. The borrow amount is converted into BTC automatically.
+        </p>
+        <div className="is-flex is-justify-content-space-between">
+          <div>
+            <p className="is-secondary-purple has-text-weight-bold is-size-7">
+              Max Multiple
+            </p>
+            <h3 className="is-purple">4.33x</h3>
+          </div>
+          <div>
+            <p className="is-secondary-purple has-text-weight-bold is-size-7 has-text-right">
+              Redeem & Swap fee
+            </p>
+            <h3 className="is-purple has-text-right">0.25%</h3>
+          </div>
+        </div>
+        <p className="has-text-centered mt-2">
+          <Link href={`/multiply/btc-long`}>
+            <a className="button is-primary ml-3">Multiply</a>
+          </Link>
+        </p>
+      </div>
+      <style jsx>{`
+        .is-box {
+          max-width: 400px;
+        }
+        .icon-container {
+          margin-bottom: -10px;
+          position: relative;
+          top: -42px;
+        }
+        .button {
+          min-width: 100%;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default MultiplyOffer
--- pages/multiply/[asset].tsx
+++ pages/multiply/[asset].tsx
@@ -1,41 +1,32 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
-import Splash from 'components/multiply/splash'
 import Multiply from 'components/multiply'
 import { useRouter } from 'next/router'
 import { Offer } from 'lib/types'
-import { fetchOffers } from 'lib/api'
+import { fetchOffer } from 'lib/api'
 import SomeError from 'components/layout/error'
 import Spinner from 'components/spinner'
 
 const MultiplyAsset: NextPage = () => {
   const router = useRouter()
   const { asset } = router.query
-  const [multiply, setMultiply] = useState(false)
-  const [offers, setOffers] = useState<Offer[]>()
+  const [offer, setOffer] = useState<Offer>()
   const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
     setLoading(true)
-    fetchOffers().then((data) => {
-      setOffers(data)
-      setLoading(false)
-    })
-  }, [])
+    if (asset === 'btc-long') {
+      fetchOffer('fUSD', 'LBTC').then((data) => {
+        setOffer(data)
+        setLoading(false)
+      })
+    }
+  }, [asset])
 
   if (isLoading) return <Spinner />
-  if (!offers) return <SomeError>Error getting offers</SomeError>
-
-  const offer = offers.find(
-    ({ synthetic, collateral }) =>
-      synthetic.ticker === asset && collateral.ticker === 'LBTC'
-  )
-
   if (!offer) return <SomeError>Error getting offer</SomeError>
 
-  if (multiply) return <Multiply offer={offer} />
-  return <Splash click={() => setMultiply(true)} />
+  return <Multiply offer={offer} />
 }
 
 export default MultiplyAsset
-
