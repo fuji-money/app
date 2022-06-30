c8ddf7042521fc6bedbc781305bace6b9d74062f
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-06-30 10:32:05 +0000

    adds issuer and oracle pubkeys to constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,5 @@
 export const minMultiplyRatio = 130
 export const maxMultiplyRatio = 330
+
+export const issuerPubKey = '02a3d89442c53fa319737bce93e0408d00f817f992a276fea1a2aa0ffbdc4a8a76'
+export const oraclePubKey = '0256e332a5134f31dbea899e0cb7c75d3e2cff969d3958d066f8198caaee3a6159'
