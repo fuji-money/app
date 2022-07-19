4418d4ba54e426629b7ec8b07c4f093f5a92f422
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-07-19 10:47:20 +0000

    use constant on message to user (create account)

--- components/modals/account.tsx
+++ components/modals/account.tsx
@@ -1,12 +1,14 @@
 import Spinner from 'components/spinner'
 import Modal from './modal'
+import { marinaFujiAccountID } from 'lib/constants'
 
 const AccountModal = () => {
   return (
     <Modal id="account-modal">
       <Spinner />
       <h3 className="mt-4">Waiting for confirmation...</h3>
-      <p>Creating a Marina account named <strong>fuji</strong></p>
+      <p>Creating a Marina account named <strong>{marinaFujiAccountID}</strong></p>
+      <p>Please accept and unlock on Marina</p>
     </Modal>
   )
 }
