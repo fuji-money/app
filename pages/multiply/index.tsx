4a7f7bd2f94161acd7a8ad35394b8847bbfe7e09
Miyo <100925541+miyo-fuji@users.noreply.github.com>
2022-07-26 13:08:43 +0000

    multiply copy

--- pages/multiply/index.tsx
+++ pages/multiply/index.tsx
@@ -4,13 +4,9 @@ import type { NextPage } from 'next'
 const MultiplyPage: NextPage = () => {
   return (
     <section>
-      <h1>FUJI Multiply</h1>
-      <p className="is-size-7 mt-6">
-        In vivamus mi pretium pharetra cursus lacus, elit. Adipiscing eget vel
-        ut non duis vitae. Augue mi, bibendum ac imperdiet ipsum sed ornare.
-        Facilisis id sem quam elementum euismod ante ut. Ac, pharetra elit, sit
-        pharetra a. Eu diam nunc nulla risus arcu, integer nulla diam, est. Nisl
-        accumsan potenti mattis consectetur pellentesque.
+      <h1>Multiply</h1>
+      <p className="is-size-12 mt-6 has-text-centered">
+        Multiply your exposure to your Bitcoin and other digital assets. Browse our available products below.
       </p>
       <MultiplyOffer />
       <style jsx>{`
