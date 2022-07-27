fdac43a2fae3328501abac985a22d3be856373f2
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-07-27 15:38:30 +0000

    stash wip

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,10 @@
-export const hashEmail = (email: string) => `${email}hashed` // TODO
+import Cookies from 'cookies'
+
+export const hashEmail = (email: string) => `${email}/hashed` // TODO
+
+export const setCookie = (email: string, req: any, res: any) => {
+  const cookies = new Cookies(req, res)
+  cookies.set('myCookieName', hashEmail, {
+    httpOnly: true // true by default
+  })
+}
