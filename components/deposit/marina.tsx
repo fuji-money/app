61211c5691d6a2e80b53907de0424c0a65e11cdf
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-07-28 12:12:00 +0000

    show error on failure

--- components/deposit/marina.tsx
+++ components/deposit/marina.tsx
@@ -11,11 +11,12 @@ import { NetworkContext } from 'components/providers/network'
 
 interface MarinaProps {
   contract: Contract
-  topup: number | undefined
+  setError: any
   setResult: any
+  topup: number | undefined
 }
 
-const Marina = ({ contract, topup, setResult }: MarinaProps) => {
+const Marina = ({ contract, setError, setResult, topup }: MarinaProps) => {
   const { ticker, value } = contract.collateral
   const [ step, setStep ] = useState(0)
   const quantity = topup || contract.collateral.quantity
@@ -36,7 +37,8 @@ const Marina = ({ contract, topup, setResult }: MarinaProps) => {
                 contract.network = network
                 addContract(contract) // add to local storage TODO
                 setResult('success')
-              } catch(_) {
+              } catch(error) {
+                setError(error)
                 setResult('failure')
               }
             }}
--- components/deposit/result.tsx
+++ components/deposit/result.tsx
@@ -13,14 +13,7 @@ const Success = () => {
         />
       </p>
       <h2 className="mt-4">Success</h2>
-      <p className="is-size-7 has-text-left">
-        In vivamus mi pretium pharetra cursus lacus, elit. Adipiscing eget vel
-        ut non duis vitae. Augue mi, bibendum ac imperdiet ipsum sed ornare.
-        Facilisis id sem quam elementum euismod ante ut. Ac, pharetra elit, sit
-        pharetra a. Eu diam nunc nulla risus arcu, integer nulla diam, est. Nisl
-        accumsan potenti mattis consectetur pellentesque.
-      </p>
-      <p className="has-text-centered mt-4">
+      <p className="has-text-centered mt-5">
         <Link href="/dashboard">
           <a className="button is-primary is-cta">Back to dashboard</a>
         </Link>
@@ -29,7 +22,7 @@ const Success = () => {
   )
 }
 
-const Failure = () => {
+const Failure = ({ error }: { error: any }) => {
   return (
     <div className="has-text-centered mx-6">
       <p>
@@ -41,17 +34,8 @@ const Failure = () => {
         />
       </p>
       <h2 className="mt-4">Something went wrong</h2>
-      <p className="is-size-7 has-text-left">
-        In vivamus mi pretium pharetra cursus lacus, elit. Adipiscing eget vel
-        ut non duis vitae. Augue mi, bibendum ac imperdiet ipsum sed ornare.
-        Facilisis id sem quam elementum euismod ante ut. Ac, pharetra elit, sit
-        pharetra a. Eu diam nunc nulla risus arcu, integer nulla diam, est. Nisl
-        accumsan potenti mattis consectetur pellentesque.
-      </p>
-      <p className="has-text-centered mt-4">
-        <a className="button is-primary is-cta">Retry deposit</a>
-      </p>
-      <p className="has-text-centered mt-4">
+      <p className="is-size-7 mt-4">{`${error}`}</p>
+      <p className="has-text-centered mt-5">
         <Link href="/dashboard">
           <a className="button is-cta">Back to dashboard</a>
         </Link>
@@ -61,12 +45,13 @@ const Failure = () => {
 }
 
 interface ResultProps {
+  error: any
   result: string
 }
 
-const Result = ({ result }: ResultProps) => {
+const Result = ({ error, result }: ResultProps) => {
   if (result === 'success') return <Success />
-  if (result === 'failure') return <Failure />
+  if (result === 'failure') return <Failure error={error} />
   return <></>
 }
 
