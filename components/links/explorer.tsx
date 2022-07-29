05141bb4edbd220e1d793e5953229a612eca97d8
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-07-29 09:49:27 +0000

    Use liquid.network instead of blockstream

--- components/links/explorer.tsx
+++ components/links/explorer.tsx
@@ -1,9 +1,18 @@
+import { NetworkContext } from 'components/providers/network'
+import { useContext } from 'react'
+
 interface ExplorerLinkProps {
   txid: string
 }
 
 const ExplorerLink = ({ txid }: ExplorerLinkProps) => {
-  const href = `https://blockstream.info/liquidtestnet/tx/${txid}` // TODO
+  const { network } = useContext(NetworkContext)
+
+  const href =
+    network === 'testnet'
+      ? `https://liquid.network/testnet/tx/${txid}`
+      : `https://liquid.network/tx/${txid}`
+
   return (
     <a href={href} className="button external" target="_blank" rel="noreferrer">
       🔗 Open in explorer
