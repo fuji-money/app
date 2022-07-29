4e971822c646ddce49d1046321b0c5709d0bc375
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-07-29 09:29:35 +0000

    If Marina not installed, show "Install Marina" text in the button and redirect to vulpem.com/marina #19

--- components/buttons/connect.tsx
+++ components/buttons/connect.tsx
@@ -1,20 +1,26 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { WalletContext } from 'components/providers/wallet'
 import { createFujiAccount, getMarina, fujiAccountMissing } from 'lib/marina'
 import { closeModal, openModal } from 'lib/utils'
 import AccountModal from 'components/modals/account'
+import { MarinaProvider } from 'marina-provider'
 
 const ConnectButton = () => {
   const { connect, disconnect, wallet } = useContext(WalletContext)
+  const [marina, setMarina] = useState<MarinaProvider>()
+
+  useEffect(() => {
+    const checkMarina = async () => setMarina(await getMarina())
+    checkMarina()
+  })
 
   const toggle = async () => {
-    const marina = await getMarina()
     if (!marina) return
     if (wallet) {
       await marina.disable()
       disconnect()
     } else {
-      if (!await marina.isEnabled()) await marina.enable()
+      if (!(await marina.isEnabled())) await marina.enable()
       if (await fujiAccountMissing(marina)) {
         openModal('account-modal')
         await createFujiAccount(marina)
@@ -28,10 +34,24 @@ const ConnectButton = () => {
 
   return (
     <>
-      <button onClick={toggle} className="button is-primary my-auto">
-        {message}
-      </button>
-      <AccountModal />
+      {marina && (
+        <>
+          <button onClick={toggle} className="button is-primary my-auto">
+            {message}
+          </button>
+          <AccountModal />
+        </>
+      )}
+      {!marina && (
+        <a
+          href="https://vulpem.com/marina"
+          target="_blank"
+          rel="noreferrer"
+          className="button is-primary my-auto"
+        >
+          Install Marina
+        </a>
+      )}
     </>
   )
 }
