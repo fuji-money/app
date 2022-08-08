be46b4d2e16fd3c47e76effd762d3bc5d5d10c4e
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-08-08 10:34:57 +0000

    implements auth based on viral loop campaign

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { calcAuthCookie } from 'lib/auth'
+import { fetchURL } from 'lib/fetch'
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,10 +10,18 @@ export default async function handler(
   if (req.method !== 'POST' || !req.body) return res.status(405).end()
 
   const email = req.body
-  const vipList = JSON.parse(process.env.VIP_LIST || '[]')
+  const maxOrder = process.env.VIRAL_LOOP_MAX_ORDER
+  const publicToken = process.env.VIRAL_LOOP_PUBLIC_TOKEN
+
+  const url =
+    'https://app.viral-loops.com/api/v3/campaign/participant/order?' +
+    `publicToken=${publicToken}&` +
+    `email=${email}`
+
+  const data = await fetchURL(url)
 
   // if valid email, return cookie value to be set client side
-  if (vipList.includes(email))
+  if (maxOrder && data.order <= maxOrder)
     return res.status(200).json(calcAuthCookie(email))
 
   // email not in vip list, return 401
