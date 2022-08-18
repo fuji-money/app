1d707fb04c4f0a6f83fb767df6f392ef9d6e0450
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-08-18 15:38:35 +0100

    Adds price level hint to ratio range selector (#62)
    * - adds price level hint to ratio range selector
    - changes function getContractPriceLevel() to accept an asset instead of a contract
    - new function prettyPriceLevel() on lib/pretty
    - refactor how X coordinate is calculated on ratio range selector
    
    * prettier
    
    * change color of price level hint depending on state

--- lib/contracts.ts
+++ lib/contracts.ts
@@ -1,4 +1,4 @@
-import { Contract, ContractState } from './types'
+import { Asset, Contract, ContractState } from './types'
 import Decimal from 'decimal.js'
 import { toSatoshis } from './utils'
 import { getNetwork } from './marina'
@@ -68,13 +68,9 @@ export const getContractPayoutAmount = (
 }
 
 // get contract price level
-export const getContractPriceLevel = (
-  contract: Contract,
-  ratio: number,
-): number => {
-  const { collateral } = contract
-  if (!collateral.ratio) throw new Error('Collateral without minimum ratio')
+export const getContractPriceLevel = (asset: Asset, ratio: number): number => {
+  if (!asset.ratio) throw new Error('Asset without minimum ratio')
   return Decimal.ceil(
-    Decimal.mul(collateral.value, collateral.ratio).div(ratio),
+    Decimal.mul(asset.value, asset.ratio).div(ratio),
   ).toNumber()
 }
