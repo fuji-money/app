22c030e4ec8bf15301c9597a3e1006299aa53a14
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-12-06 11:57:38 +0000

    add Close button to webln modal

--- components/modals/webln.tsx
+++ components/modals/webln.tsx
@@ -1,4 +1,5 @@
 import { WalletContext } from 'components/providers/wallet'
+import { closeModal } from 'lib/utils'
 import { useContext } from 'react'
 import Modal, { ModalIds } from './modal'
 
@@ -7,9 +8,16 @@ const WeblnModal = () => {
   return (
     <Modal id={ModalIds.Webln}>
       <h3 className="mt-4">WebLN provider detected</h3>
-      <button className="button is-primary" onClick={enableWeblnHandler}>
-        Enable it
-      </button>
+      <p className="mt-6">
+        <button className="button" onClick={() => closeModal(ModalIds.Webln)}>
+          Close
+        </button>
+        &nbsp;
+        <button className="button is-primary" onClick={enableWeblnHandler}>
+          Enable it
+        </button>
+      </p>
+      <p className="confirm">You can enable it later on settings</p>
     </Modal>
   )
 }
