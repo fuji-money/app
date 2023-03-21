63246e67221a1836c4ff22bf50d3b7ba0f838c0e
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2023-03-21 17:28:19 +0000

    Removes auth from app
    The following env variables can be undefined (not used anymore):
    
    ENCRYPTION_SECRET
    VIP_LIST
    VIRAL_LOOP_SECRET_TOKEN
    VIRAL_LOOP_MAX_RANK

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,12 @@
-import InvalidEmailModal from 'components/modals/invalidEmail'
-import { setAuthCookie } from 'lib/auth'
-import { postData } from 'lib/fetch'
-import { openModal } from 'lib/utils'
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import Router from 'next/router'
-import { useEffect } from 'react'
 import Script from 'next/script'
-import { ModalIds } from 'components/modals/modal'
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const validUser = await postData('/api/login/check', document.cookie)
-        if (validUser) Router.push('/dashboard')
-      } catch (ignore) {}
-    }
-    checkAuth()
-    // this now gets called when the component unmounts
-    return () => {}
-  }, [])
-
-  const handleSubmit = async (event: any) => {
-    event.preventDefault()
-    const email = event.target.email.value
-    if (!email) return
-
-    // check if email is valid, gets cookie value
-    const res = await fetch('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(email),
-    })
-
-    if (res.ok) {
-      const cookie = await res.json()
-      setAuthCookie(cookie)
-      Router.push('/dashboard')
-    } else {
-      console.info('Email not in short list')
-      openModal(ModalIds.InvalidEmail)
-      // deleteCookie()
-    }
-  }
-
   return (
     <section>
-      <div className="has-text-centered">
+      <div className="is-flex is-flex-direction-column is-align-items-center">
         <p className="mt-6">
           <Image
             className="logo"
@@ -57,31 +16,31 @@ const Home: NextPage = () => {
             height="128"
           />
         </p>
-        <p className="intro mt-6">Get access to the Closed Beta</p>
-        <form onSubmit={handleSubmit}>
-          <input name="email" type="email" placeholder="me@email.com" />
-          <br />
-          <br />
-          <button className="button is-primary">Enter</button>
-        </form>
+        <p className="has-text-centered intro mt-6">
+          Choose any link below to access the decentralized web app
+        </p>
+        <button className="button" onClick={() => Router.push('/dashboard')}>
+          fuji.money
+        </button>
+        <button className="button" disabled>
+          tryfuji.com
+        </button>
+        <button className="button" disabled>
+          fujiapp.io
+        </button>
       </div>
-      <InvalidEmailModal />
       <style jsx>{`
-        input {
-          border: 2px solid #88389d;
-          border-radius: 96px;
-          color: #88389d;
-          line-height: inherit;
-          min-width: 200px;
-          padding: 10px;
+        .button {
+          margin-bottom: 1rem;
+          width: 420px;
         }
-        .links-.intro {
+        .intro {
           color: #2c024d;
           display: inline-block;
-          font-size: 1.2rem;
+          font-size: 1.5rem;
           font-weight: 400;
           margin: 1em auto;
-          max-width: 500px;
+          width: 420px;
         }
         .logo {
           display: block;
