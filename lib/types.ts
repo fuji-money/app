a560a8000e8ea8d71f688b851bec490576526839
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2023-04-05 10:35:08 +0000

    lint

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,6 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import { TxOutput } from 'liquidjs-lib'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
