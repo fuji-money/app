af88c9242bef7c458ab912da8932ede68a74a6be
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2023-05-16 12:37:40 +0000

    fix issue #137: clicking on closed in dashboard on contracts list does not switch to it

--- components/contracts/list.tsx
+++ components/contracts/list.tsx
@@ -6,7 +6,6 @@ import { WalletContext } from 'components/providers/wallet'
 import ContractRow from './row'
 import Spinner from 'components/spinner'
 import { ContractsContext } from 'components/providers/contracts'
-import { ModalStages } from 'components/modals/modal'
 
 interface ContractsListProps {
   showActive: boolean
@@ -24,8 +23,7 @@ const ContractsList = ({ showActive }: ContractsListProps) => {
         showActive ? !contractIsClosed(contract) : contractIsClosed(contract),
       ),
     )
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [contracts])
+  }, [contracts, showActive])
 
   if (!connected)
     return (
