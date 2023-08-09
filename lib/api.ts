4c1e53df25c6f13cd4c0461ff7db79cfad730032
Louis Singer <41042567+louisinger@users.noreply.github.com>
2023-08-10 00:25:01 +0200

    remove `NEXT_PUBLIC_LIQUIDATOR_PUBKEY` and `NEXT_PUBLIC_FUSD_ASSET_ID` (#159)
    * remove en vars (synth asset ID & liquidator public key), fetch them from backend
    
    * remove unused import

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import { NetworkString } from 'marina-provider'
 import { fetchURL } from './fetch'
-import { Investment, Oracle, Stock } from './types'
+import { ConfigResponse, Investment, Oracle, Stock } from './types'
 import { factoryUrlMainnet, factoryUrlTestnet } from './constants'
 
 export async function fetchInvestments(
@@ -17,8 +17,10 @@ export function getFactoryUrl(network: NetworkString) {
   return network === 'liquid' ? factoryUrlMainnet : factoryUrlTestnet
 }
 
-export async function fetchConfig(network: NetworkString) {
-  return await fetchURL(getFactoryUrl(network) + '/contracts/info')
+export async function fetchConfig(
+  network: NetworkString,
+): Promise<ConfigResponse> {
+  return fetchURL(getFactoryUrl(network) + '/contracts/info')
 }
 
 export const getBTCvalue = async (oracle: Oracle): Promise<number> => {
--- pages/contracts/[txid]/topup/lightning.tsx
+++ pages/contracts/[txid]/topup/lightning.tsx
@@ -175,6 +175,7 @@ const ContractTopupLightning: NextPage = () => {
           network,
           collateralUtxos,
           oracles[0],
+          config.xOnlyTreasuryPublicKey,
         )
 
         // propose contract to alpha factory
