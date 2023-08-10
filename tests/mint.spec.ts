e1de812a9bd85cf793a54f2a1a6c15d9747bd9ea
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2023-08-10 14:26:13 +0000

    add playwright tests to multiply

--- tests/mint.spec.ts
+++ tests/mint.spec.ts
@@ -10,7 +10,7 @@ import { DEFAULT_LIGHTNING_LIMITS } from '../lib/swaps'
 
 // this test does not go through the entire mint flow (waiting for a regtest config to do this)
 // it only checks that the mint page is accessible
-test('connect marina & use the mint page', async ({
+test('connect marina & use the mint and multiply page', async ({
   page,
   extensionId,
   context,
@@ -39,9 +39,10 @@ test('connect marina & use the mint page', async ({
   await page.getByRole('button', { name: 'Mint' }).click()
   await page.waitForSelector('text=Borrow')
 
-  const button = page.getByRole('button', { name: 'Proceed to Deposit' })
-  const amount = page.getByPlaceholder('0.00')
-  const slider = page.getByRole('slider')
+  let button = page.getByRole('button', { name: 'Proceed to Deposit' })
+  let amount = page.getByPlaceholder('0.00')
+  let slider = page.getByRole('slider')
+  let oracle = page.getByText('Fuji.Money', { exact: true })
 
   // test lightning out of bounds (> maximal)
   await amount.fill((DEFAULT_LIGHTNING_LIMITS.maximal + 1).toString())
@@ -57,4 +58,39 @@ test('connect marina & use the mint page', async ({
   // expect to find warning about unsafe ratio
   await slider.fill('121')
   await expect(page.getByText('ratio is unsafe')).toBeVisible()
+
+  // expect warning about not enough oracles
+  await oracle.click()
+  await expect(page.getByText('not enough oracles')).toBeVisible()
+
+  // go to multiply page
+  await page.getByRole('link', { name: 'Multiply' }).click()
+  await page.waitForSelector('text=BTC-LONG')
+  await page.getByRole('button', { name: 'Multiply' }).click()
+  await page.waitForSelector('text=Deposit your L-BTC', { state: 'visible' })
+
+  button = page.getByRole('button', { name: 'Multiply' })
+  amount = page.getByPlaceholder('0.00')
+  slider = page.getByRole('slider')
+  oracle = page.getByText('Fuji.Money', { exact: true })
+
+  // test lightning out of bounds (> maximal)
+  await amount.fill((DEFAULT_LIGHTNING_LIMITS.maximal + 1).toString())
+  await expect(button).toBeDisabled()
+  await expect(page.getByText(/out of bounds/i)).toBeVisible()
+  await expect(page.getByText(/not enough funds/i)).toBeVisible()
+
+  // test lightning out of bounds (< minimal)
+  await amount.fill((DEFAULT_LIGHTNING_LIMITS.minimal - 1).toString())
+  await expect(button).toBeDisabled()
+  await expect(page.getByText(/out of bounds/)).toBeVisible()
+
+  // expect to find warning about unsafe ratio
+  await slider.fill('99') // a dangerous level for the ratio
+  await slider.dispatchEvent('mouseup') // slider updates ratio on mouse up
+  await expect(page.getByText('ratio is unsafe')).toBeVisible()
+
+  // expect warning about not enough oracles
+  await oracle.click()
+  await expect(page.getByText('not enough oracles')).toBeVisible()
 })
