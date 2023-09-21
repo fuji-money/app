44c803bbe63d02f1ba17599b5dcb3bbf9ab6ae27
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2023-09-21 16:08:21 +0000

    change trade.fuji.money url para from asset to asset_p

--- components/buttons/trade.tsx
+++ components/buttons/trade.tsx
@@ -2,7 +2,7 @@ const TradeButton = ({ hash }: { hash: string }) => {
   return (
     <a
       className="button"
-      href={`https://trade.fuji.money/?asset=${hash}`}
+      href={`https://trade.fuji.money/?asset_p=${hash}`}
       target="_blank"
       rel="noreferrer"
     >
