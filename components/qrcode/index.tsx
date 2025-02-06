98b0e92d6f8964a6cf0c27bf7509c369f40be6dc
tiero <3596602+tiero@users.noreply.github.com>
2025-02-06 19:05:06 +0100

    fix qrcode

--- components/qrcode/index.tsx
+++ components/qrcode/index.tsx
@@ -26,7 +26,7 @@ function QRCode({ text }: QRCodeProps) {
         options={{
           type: 'image/jpeg',
           quality: 0.3,
-          errorCorrectionLevel: 'M',
+          level: 'M',
           margin: 0,
           scale: 4,
           width: 300,
